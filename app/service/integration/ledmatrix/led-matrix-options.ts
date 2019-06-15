8182cd3d7ddacc65fbd07027f3dac6fdbe3f1743
Jacek Gołębiewski <jacekg1993@gmail.com>
2019-06-15 15:01:01 +0200

    Package json update and small fix

--- app/service/integration/ledmatrix/led-matrix-options.ts
+++ app/service/integration/ledmatrix/led-matrix-options.ts
@@ -3,6 +3,6 @@ export class LedMatrixOptions {
     constructor(
         public device: string = '/dev/spidev0.0', //'/dev/spidev0.0'
         public controllerCount: number = 1,
-        public flip?: 'none' //none, vertical, horizontal, both
+        public flip: string = 'none'  //none, vertical, horizontal, both
     ) {}
 }
