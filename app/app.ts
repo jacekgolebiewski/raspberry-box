89ba412057388a27418c849b0234b4074e2461e4
Jacek Gołębiewski <jacekg1993@gmail.com>
2019-06-23 12:41:11 +0200

    freeze fix try

--- app/app.ts
+++ app/app.ts
@@ -13,7 +13,7 @@ export class App {
         gpio.on('change', function(channel, value) {
             console.log('Channel ' + channel + ' value is now ' + value);
         });
-        gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
+        gpio.setup(36, gpio.DIR_IN, gpio.EDGE_BOTH);
     }
     /*
     async start() {
