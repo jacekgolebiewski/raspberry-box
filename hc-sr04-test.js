4a1537ba82988975371655e3c3fd493e7d0d803b
Jacek Gołębiewski <jacekg1993@gmail.com>
2019-06-23 16:01:16 +0200

    hcsr tests

--- hc-sr04-test.js
+++ hc-sr04-test.js
@@ -3,82 +3,15 @@ const ECHO = 36;
 const TRIG = 37;
 const BUTTON = 38;
 
-gpio.setup(TRIG, gpio.DIR_OUT);
-gpio.setup(ECHO, gpio.DIR_IN, gpio.EDGE_BOTH);
-gpio.setup(BUTTON, gpio.DIR_IN, gpio.EDGE_BOTH);
 
+var usonic = require('mmm-usonic-fixed');
 
-async function readPin(pin) {
-    return new Promise((resolve, reject) => {
-        gpio.read(pin, function(err, value) {
-            if (err) throw err;
-            resolve(pin);
-        });
-    });
-}
-
-async function checkDistance() {
-    gpio.on('change', (channel, value) => {
-        if(channel === BUTTON) {
-            console.log(`TRIG ${value}`);
-            gpio.write(TRIG, value);
-        } else {
-            console.log(`Poll ${channel} val: ${value}`);
-        }
-    });
-/*
-
-    console.log('checkDistance()');
-    gpio.write(TRIG, true);
-    for(let i = 0; i< 1000; i++) {
-        console.log('Waiting...');
+usonic.init(function (error) {
+    if (error) {
+        console.log(error);
+    } else {
+        var sensor = usonic.createSensor(ECHO, TRIG, 750);
+        console.log(sensor());
     }
-    gpio.write(TRIG, false);
-    console.log('triggered');
-*/
+});
 
-    while (true) {
-        let value = await readPin(ECHO);
-        if (value === true) {
-            break;
-        }
-    }
-/*    let startDate = new Date();
-    console.log('found 1 on ECHO pin');
-
-    while (true) {
-        let value = await readPin(ECHO);
-        if (value === false) {
-            break;
-        }
-    }
-    let endDate = new Date();
-    console.log('found 0 on ECHO pin');
-
-    console.log(`${startDate.getTime()} / ${endDate.getTime()}`);*/
-}
-/*
-    gpio.on('change', (channel, value) => {
-
-    });
-
-
-    var start = new Date()
-    var hrstart = process.hrtime()
-    var simulateTime = 5
-
-    setTimeout(function(argument) {
-        // execution time simulated with setTimeout function
-        var end = new Date() - start,
-            hrend = process.hrtime(hrstart)
-
-        console.info('Execution time: %dms', end)
-        console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000)
-    }, simulateTime)*/
-
-
-
-setTimeout(() => {
-    console.log('Checking distance...');
-    checkDistance();
-}, 1000);
