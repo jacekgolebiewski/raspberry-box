b3379916d7e3040bd0d8ce46ebf6e37c33f9ee03
Jacek Gołębiewski <jacekg1993@gmail.com>
2019-06-23 19:24:14 +0200

    hcsr with rpi-gpio test

--- hc-sr04-test.js
+++ hc-sr04-test.js
@@ -1,4 +1,5 @@
 const gpio = require('rpi-gpio');
+const gpiop = gpio.promise;
 
 const ECHO = 36;
 const TRIG = 37;
@@ -7,10 +8,14 @@ const BUTTON = 38;
 gpio.setup(TRIG, gpio.DIR_OUT);
 gpio.setup(ECHO, gpio.DIR_IN, gpio.EDGE_BOTH);
 gpio.setup(BUTTON, gpio.DIR_IN, gpio.EDGE_BOTH);
+setTimeout(() => {
+    gpiop.read(ECHO, (err, val) => {
+        console.log('READ '+val);
+    });
+}, 1000);
 
-gpio.read(ECHO, (err, val) => {
-    console.log('READ '+val);
-});
+
+/*
 
 async function readPin(pin) {
     return new Promise((resolve, reject) => {
@@ -30,7 +35,7 @@ async function checkDistance() {
             console.log(`Poll ${channel} val: ${value}`);
         }
     });
-    /*
+    /!*
 
         console.log('checkDistance()');
         gpio.write(TRIG, true);
@@ -39,7 +44,7 @@ async function checkDistance() {
         }
         gpio.write(TRIG, false);
         console.log('triggered');
-    */
+    *!/
 
     while (true) {
         let value = await readPin(ECHO);
@@ -47,7 +52,7 @@ async function checkDistance() {
             break;
         }
     }
-    /*    let startDate = new Date();
+    /!*    let startDate = new Date();
         console.log('found 1 on ECHO pin');
 
         while (true) {
@@ -59,12 +64,13 @@ async function checkDistance() {
         let endDate = new Date();
         console.log('found 0 on ECHO pin');
 
-        console.log(`${startDate.getTime()} / ${endDate.getTime()}`);*/
+        console.log(`${startDate.getTime()} / ${endDate.getTime()}`);*!/
 }
 
 checkDistance();
-/*
+/!*
     gpio.on('change', (channel, value) => {
 
-    });*/
+    });*!/
 
+*/
