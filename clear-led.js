acd33449439746699f643a000c5d5f25fd63f2ba
Jacek Gołębiewski <jacekg1993@gmail.com>
2019-06-23 18:44:02 +0200

    led clear

--- clear-led.js
+++ clear-led.js
@@ -1,6 +1,8 @@
-let Max7219 = require('max7219-display');
-new Max7219({
+const Max7219 = require('max7219-display');
+const matrix = new Max7219({
     device: '/dev/spidev0.0',
     controllerCount: 1,
     flip: 'none'
-}).clear(0);
+});
+matrix.reset(0);
+matrix.clear(0);
