430bcc1c149afe947f8483a9971509c6130993e6
Jacek Gołębiewski <jacekg1993@gmail.com>
2019-06-23 12:21:14 +0200

    freeze fix try

--- app/app.ts
+++ app/app.ts
@@ -9,12 +9,28 @@ import { WebSocketEndpoint } from './api/web-socket-endpoint';
 import { Application } from './api/application';
 
 export class App {
+/*
 
     @Inject private application: Application;
     @Inject private webSocketEndpoint: WebSocketEndpoint;
     @Inject private gpioService: GpioService;
     @Inject private systemService: SystemService;
+*/
 
+    async start() {
+        const rpio = require('rpio')
+        rpio.init({
+            gpiomem: true,
+            mapping: 'physical'
+        });
+
+        console.log('Started...');
+        rpio.open(36, rpio.INPUT, rpio.PULL_DOWN);
+        rpio.poll(36, function (pin) {
+            console.log('Pressed button ' + pin);
+        });
+    }
+    /*
     async start() {
         Logger.critical(`Running with Environment.${EnvironmentUtil.currentEnvironment}`);
         AppBanner.printTitle();
@@ -41,7 +57,7 @@ export class App {
             this.webSocketEndpoint = new WebSocketEndpoint();
             this.webSocketEndpoint.init();
         }, 1000);
-    }
+    }*/
 }
 
 export default new App()
