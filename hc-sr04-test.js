9ab83ddf26843dbb88c52928f385b1e57e9ea994
Jacek Gołębiewski <jacekg1993@gmail.com>
2019-06-23 19:11:02 +0200

    hcsr with rpi-gpio test

--- hc-sr04-test.js
+++ hc-sr04-test.js
@@ -2,15 +2,62 @@ const ECHO = 36;
 const TRIG = 37;
 const BUTTON = 38;
 
+gpio.setup(TRIG, gpio.DIR_OUT);
+gpio.setup(ECHO, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(BUTTON, gpio.DIR_IN, gpio.EDGE_BOTH);
 
-const usonic = require('mmm-usonic-fixed');
 
-usonic.init(function (error) {
-    if (error) {
-        console.log(error);
-    } else {
-        var sensor = usonic.createSensor(ECHO, TRIG, 750);
-        console.log(sensor());
+async function readPin(pin) {
+    return new Promise((resolve, reject) => {
+        gpio.read(pin, function(err, value) {
+            if (err) throw err;
+            resolve(pin);
+        });
+    });
+}
+
+async function checkDistance() {
+    gpio.on('change', (channel, value) => {
+        if(channel === BUTTON) {
+            console.log(`TRIG ${value}`);
+            gpio.write(TRIG, value);
+        } else {
+            console.log(`Poll ${channel} val: ${value}`);
+        }
+    });
+    /*
+
+        console.log('checkDistance()');
+        gpio.write(TRIG, true);
+        for(let i = 0; i< 1000; i++) {
+            console.log('Waiting...');
+        }
+        gpio.write(TRIG, false);
+        console.log('triggered');
+    */
+
+    while (true) {
+        let value = await readPin(ECHO);
+        if (value === true) {
+            break;
+        }
     }
-});
+    /*    let startDate = new Date();
+        console.log('found 1 on ECHO pin');
+
+        while (true) {
+            let value = await readPin(ECHO);
+            if (value === false) {
+                break;
+            }
+        }
+        let endDate = new Date();
+        console.log('found 0 on ECHO pin');
+
+        console.log(`${startDate.getTime()} / ${endDate.getTime()}`);*/
+}
+/*
+    gpio.on('change', (channel, value) => {
+
+    });*/
 
